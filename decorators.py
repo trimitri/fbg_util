238e930dff1f27657fba1987b828c41758684174
Franz Gutsch <franz.balthasar+github@gmail.com>
2018-05-24 12:24:05 +0200

    Some comment on how to static variable.

--- decorators.py
+++ decorators.py
@@ -5,7 +5,11 @@ from typing import Any, Callable
 
 def static_variable(variable_name: str,
                     initial_value: Any) -> Callable[[Callable], Callable]:
-    """A decorator to add a static variable to a function."""
+    """A decorator to add a static variable to a function.
+
+    Inside the decorated function, the variable can be accessed like
+    `funcname.foovar`.
+    """
     def decorator(function):
         setattr(function, variable_name, initial_value)
         return function
